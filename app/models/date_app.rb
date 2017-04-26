e7b5cd18d180385c38c2fdff8050165024b3e5fb
Federico Strauch <fedestrauch@gmail.com>
2017-04-26 19:53:11 -0400

    logic fixed

--- app/models/date_app.rb
+++ app/models/date_app.rb
@@ -9,17 +9,20 @@ class DateApp < ApplicationRecord
   def check_for_matches
     #get the topic I just picked
     last_pick = self.topics.last
-    p "/\\/"*40
-    p last_pick
     #get all the users that dislike that topic
     users = Dislike.where(topic: last_pick).map do |dislike|
-      DateApp.find(dislike.date_app.id)
+      DateApp.find(dislike.date_app_id)
     end
+
     #see if they dislike 3 more thngs that I dislike
     users.each do | user |
       #get the users topics
-      if (user.topics & self.topics).count >= 4
-        return user
+      if user.topics.count >= 4
+        if (user.topics.count & self.topics.count) >= 4
+          return user
+        else
+          return nil
+        end
       end
       #see if 4 or more topics match mine
       #return that user
